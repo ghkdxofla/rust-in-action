a695fc7c0480d0d9508de638722d503a4a975dd3
ghkdxofla <ghkdxofla@gmail.com>
2022-09-11 09:59:43 +0900

    feat: add stdin into grep-lite

--- ch2/grep-lite/src/main.rs
+++ ch2/grep-lite/src/main.rs
@@ -1,8 +1,19 @@
 use std::fs::File;
+use std::io;
 use std::io::BufReader;
 use std::io::prelude::*;
 use regex::Regex;
 use clap::{Arg, App};
+
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
+            Some(_) => println!("{}", line),
+            None => (),
+        }
+    }
+}
 
 fn main() {
     let args = App::new("grep-lite")
@@ -21,19 +32,15 @@ fn main() {
     let pattern = args.value_of("pattern").unwrap();
     let re = Regex::new(pattern).unwrap();
 
-//     let quote = "Every face, every shop, bedroom window, public-house, and
-// dark square is a picture feverishly turned--in search of what?
-// It is the same with books. What do we seek through millions of pages?";
-
-    let input = args.value_of("input").unwrap();
-    let f = File::open(input).unwrap();
-    let reader = BufReader::new(f);
-
-    for line_ in quote.lines() {
-        let line = line_.unwrap();
-        match re.find(&line) {
-            Some(_) => println!("{}", line),
-            None => (),
-        }
+    let input = args.value_of("input").unwrap_or("-");
+    
+    if input == "-" {
+        let stdin = io::stdin();
+        let reader = stdin.lock();
+        process_lines(reader, re);
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        process_lines(reader, re);
     }
 }
