6480fc557dbf7d97c7fb725a2c49341943483d54
ghkdxofla <ghkdxofla@gmail.com>
2022-09-11 09:51:25 +0900

    feat: add ch2 codes

---
+++ ch2/grep-lite/src/main.rs
@@ -0,0 +1,39 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+use regex::Regex;
+use clap::{Arg, App};
+
+fn main() {
+    let args = App::new("grep-lite")
+        .version("0.1")
+        .about("searches for patterns")
+        .arg(Arg::with_name("pattern")
+            .help("the pattern to search for")
+            .takes_value(true)
+            .required(true))
+        .arg(Arg::with_name("input")
+            .help("File to search")
+            .takes_value(true)
+            .required(true))
+        .get_matches();
+
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
+
+//     let quote = "Every face, every shop, bedroom window, public-house, and
+// dark square is a picture feverishly turned--in search of what?
+// It is the same with books. What do we seek through millions of pages?";
+
+    let input = args.value_of("input").unwrap();
+    let f = File::open(input).unwrap();
+    let reader = BufReader::new(f);
+
+    for line_ in quote.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
+            Some(_) => println!("{}", line),
+            None => (),
+        }
+    }
+}
