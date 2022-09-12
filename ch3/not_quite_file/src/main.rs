e1b387825083ad18de59d72754d7eb1090d7dd3f
ghkdxofla <ghkdxofla@gmail.com>
2022-09-12 14:36:22 +0900

    feat: implement read function

--- ch3/not_quite_file/src/main.rs
+++ ch3/not_quite_file/src/main.rs
@@ -1,6 +1,10 @@
 #![allow(unused_variables)]
 
-type File = String;
+#[derive(Debug)]
+struct File {
+    name: String,
+    data: Vec<u8>,
+}
 
 fn open(f: &mut File) -> bool {
     true
@@ -10,14 +14,32 @@ fn close(f: &mut File) -> bool {
     true
 }
 
-#[allow(dead_code)]
-fn read(f: &mut File, save_to: &mut Vec<u8>) -> ! { // !는 Never 타입. 함수 호출 위치로 돌아오지 않으며, 중단될 것을 보장한다.
-    unimplemented!() // 중단 시키는 매크로
+fn read(
+    f: &File,
+    save_to: &mut Vec<u8>,
+) -> usize {
+    let mut tmp = f.data.clone();
+    let read_length = tmp.len();
+
+    save_to.reserve(read_length); // save_to 벡터에 읽은 데이터를 저장하기 위해 공간을 확보
+    save_to.append(&mut tmp); // save_to 벡터에 읽은 데이터를 추가
+    read_length
 }
 
 fn main() {
-    let mut f1 = File::from("f1.txt");
-    open(&mut f1);
-    //read(f1, vec![]);
-    close(&mut f1);
+    let mut f2 = File {
+        name: String::from("f2.txt"),
+        data: vec![114, 117, 115, 116, 33],
+    };
+
+    let mut buffer = vec![];
+    let f2_length = read(&f2, &mut buffer);
+    close(&mut f2);
+
+    let text = String::from_utf8_lossy(&buffer);
+
+    println!("{:?}", f2);
+    println!("{} is {} bytes long", f2.name, f2_length);
+    println!("{}", text);
+
 }
