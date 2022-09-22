8793be26d00fae240122ea2d45049e21cf81c15f
ghkdxofla <ghkdxofla@gmail.com>
2022-09-22 21:21:40 +0900

    feat: add codes

--- ch4/satellite/src/main.rs
+++ ch4/satellite/src/main.rs
@@ -1,12 +1,11 @@
 #![allow(unused_variables)]
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct CubeSat {
     id: u64,
-    mailbox: Mailbox,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 enum StatusMessage {
     Ok,
 }
@@ -16,7 +15,11 @@ struct Mailbox {
     messages: Vec<Message>,
 }
 
-type Message = String;
+#[derive(Debug)]
+struct Message {
+    to: u64,
+    content: String,
+}
 
 struct GroundStation;
 
@@ -24,24 +27,43 @@ impl GroundStation {
     fn connect(&self, sat_id: u64) -> CubeSat {
         CubeSat {
             id: sat_id,
-            mailbox: Mailbox {
-                messages: vec![],
-            },
         }
     }
     
     fn send(
         &self,
-        to: &mut CubeSat,
+        mailbox: &mut Mailbox,
         msg: Message
     ) {
-        to.mailbox.messages.push(msg);
+        mailbox.post(msg);
     }
 }
 
 impl CubeSat {
-    fn recv(&mut self) -> Option<Message> {
-        self.mailbox.messages.pop()
+    fn recv(&self, mailbox: &mut Mailbox) -> Option<Message> {
+        mailbox.deliver(&self)
+    }
+}
+
+impl Mailbox {
+    fn post(
+        &mut self,
+        msg: Message
+    ) {
+        self.messages.push(msg);
+    }
+
+    fn deliver(
+        &mut self,
+        recipient: &CubeSat
+    ) -> Option<Message> {
+        for i in 0..self.messages.len() {
+            if self.messages[i].to == recipient.id {
+                return Some(self.messages.remove(i)); // for loop 안에서 messages collection이 변경되지만, 바로 return 되므로 compiler도 문제 없다고 판단함
+            }
+        }
+
+        None
     }
 }
 
@@ -55,20 +77,25 @@ fn fetch_sat_ids() -> Vec<u64> {
 }
 
 fn main() {
+    let mut mail = Mailbox { messages: vec![] };
     let base = GroundStation {};
-    let mut sat_a = CubeSat {
-        id: 0,
-        mailbox: Mailbox { messages: vec![] },
-    };
+    let sat_ids = fetch_sat_ids();
 
-    println!("t0: {:?}", sat_a);
+    for sat_id in sat_ids {
+        let sat = base.connect(sat_id);
+        let msg = Message {
+            to: sat.id,
+            content: String::from("Hello, world!"),
+        };
 
-    base.send(&mut sat_a, Message::from("Hello there!"));
+        base.send(&mut mail, msg);
+    }
 
-    println!("t1: {:?}", sat_a);
+    let sat_ids = fetch_sat_ids();
 
-    let msg = sat_a.recv();
-    println!("t2: {:?}", sat_a);
-
-    println!("Message: {:?}", msg);
+    for sat_id in sat_ids {
+        let sat = base.connect(sat_id);
+        let msg = sat.recv(&mut mail);
+        println!("{:?}: {:?}", sat, msg);
+    }
 }
